f45fe8f1f1833f459ae920c38355865adc9e1203
ahmed <engrmahmed14@gmail.com>
2022-09-12 18:32:28 +0500

    Initial commit

---
+++ src/main/java/com/mahmedyousuf/springbootongke/SpringbootOnGkeApplication.java
@@ -0,0 +1,13 @@
+package com.mahmedyousuf.springbootongke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOnGkeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOnGkeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mahmedyousuf/springbootongke/SpringbootOnGkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmedyousuf.springbootongke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOnGkeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
